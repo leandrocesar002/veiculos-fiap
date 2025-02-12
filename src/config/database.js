3bf5cd1bb48137114d55666133931e9c0651455d
leandrocesar001 <leandro.santos@fintalk.ai>
2025-02-12 19:21:07 -0300

    fix: adjust database conection

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,13 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('postgres_fiap', 'username', 'password', {
     host: 'my-db-fiap.czmuka4aqktq.us-east-1.rds.amazonaws.com',
     dialect: 'postgres',
-    logging: false, 
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false  
+        }
+    }
 });
 
 module.exports = sequelize;
